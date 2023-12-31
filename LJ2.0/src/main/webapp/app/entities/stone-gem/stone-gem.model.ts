057efb679e269cf51918c79b1e82c5003405b955
Ces <ces@Zipcoders-MacBook-Pro-42.local>
2023-12-30 19:53:31 -0500

    test commit

--- LJ2.0/src/main/webapp/app/entities/stone-gem/stone-gem.model.ts
+++
@@ -1,9 +0,0 @@
-import { IEffects } from 'app/entities/effects/effects.model';
-
-export interface IStoneGem {
-  id: number;
-  stoneGemName?: string | null;
-  effects?: IEffects[] | null;
-}
-
-export type NewStoneGem = Omit<IStoneGem, 'id'> & { id: null };
--- LJ2/src/main/webapp/app/entities/plating-material/plating-material.model.ts
+++
@@ -1,9 +0,0 @@
-import { IEffects } from 'app/entities/effects/effects.model';
-
-export interface IPlatingMaterial {
-  id: number;
-  platingName?: string | null;
-  effects?: IEffects[] | null;
-}
-
-export type NewPlatingMaterial = Omit<IPlatingMaterial, 'id'> & { id: null };
