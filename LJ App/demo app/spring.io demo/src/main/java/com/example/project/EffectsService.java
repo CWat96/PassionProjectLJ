aee7ae2610670d6cc19ef285c50b3aa929cb6f27
Ces <ces@Zipcoders-MacBook-Pro-42.local>
2023-12-31 14:52:25 -0500

    Created effects service

---
+++ LJ App/demo app/spring.io demo/src/main/java/com/example/project/EffectsService.java
@@ -0,0 +1,45 @@
+package com.example.project;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class EffectsService {
+    private EffectsRepository repository;
+
+    @Autowired
+    public EffectsService(EffectsRepository repository) {
+        this.repository = repository;
+    }
+
+    public Effects create(Effects effects) {
+        return repository.save(effects);
+    }
+
+    public Effects readById(Long id) {
+        return repository.findById(id).get();
+    }
+
+    public List<Effects> readAll() {
+        Iterable<Effects> allEffects = repository.findAll();
+        List<Effects> effectsList = new ArrayList<>();
+        allEffects.forEach(effectsList::add);
+        return effectsList;
+    }
+
+    public Effects update(Long id, Effects newEffectsData) {
+        Effects effectsInDatabase = this.readById(id);
+        effectsInDatabase.setEffectsName(newEffectsData.getEffectsName());
+        effectsInDatabase = repository.save(newEffectsData);
+        return effectsInDatabase;
+    }
+
+    public Effects deleteById(Long id) {
+        Effects effectsToBeDeleted = this.readById(id);
+        repository.delete(effectsToBeDeleted);
+        return effectsToBeDeleted;
+    }
+}
