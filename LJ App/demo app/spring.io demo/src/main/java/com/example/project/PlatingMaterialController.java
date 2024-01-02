83ee2df1d2324d893064e524e1511f712523dd5c
Ces <ces@Zipcoders-MacBook-Pro-42.local>
2024-01-01 20:11:17 -0500

    Created PlatingMaterialController

---
+++ LJ App/demo app/spring.io demo/src/main/java/com/example/project/PlatingMaterialController.java
@@ -0,0 +1,46 @@
+package com.example.project;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+public class PlatingMaterialController {
+    private PlatingMaterialService service;
+
+    @Autowired
+    public PlatingMaterialController(PlatingMaterialService service) {
+        this.service = service;
+    }
+
+    @PostMapping(value = "/create")
+    public ResponseEntity<PlatingMaterial> create(@RequestBody PlatingMaterial platingMaterial) {
+        return new ResponseEntity<>(service.create(platingMaterial), HttpStatus.CREATED);
+    }
+
+    @GetMapping(value = "/read/{id}")
+    public ResponseEntity<PlatingMaterial> readById(@PathVariable Long id) {
+        return new ResponseEntity<>(service.readById(id), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/readAll")
+    public ResponseEntity<List<PlatingMaterial>> readAll() {
+        return new ResponseEntity<>(service.readAll(), HttpStatus.OK);
+    }
+
+    @PutMapping(value = "/update/{id}")
+    public ResponseEntity<PlatingMaterial> updateById(
+            @PathVariable Long id,
+            @RequestBody PlatingMaterial newData) {
+        return new ResponseEntity<>(service.update(id, newData), HttpStatus.OK);
+    }
+
+    @DeleteMapping(value = "/delete/{id}")
+    public ResponseEntity<PlatingMaterial> deleteById(@PathVariable Long id) {
+        return new ResponseEntity<>(service.deleteById(id), HttpStatus.OK);
+    }
+}
