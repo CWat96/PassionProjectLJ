ac5483e28385332352013b5fc221ff06787c7931
Ces <ces@Zipcoders-MacBook-Pro-42.local>
2024-01-03 22:28:25 -0500

    update

--- LJ App/demo app/spring.io demo/src/main/java/com/example/project/PlatingMaterialController.java
+++ LJ App/demo app/spring.io demo/src/main/java/com/example/project/PlatingMaterialController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
+@RequestMapping()
 public class PlatingMaterialController {
     private PlatingMaterialService service;
 
