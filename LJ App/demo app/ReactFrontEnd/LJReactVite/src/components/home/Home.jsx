5b76aa1f38862c06cbc81fc59a3cd03c669113b4
Cesily Waters <96clondon@proton.me>
2024-02-04 16:21:41 -0500

    updated DOM properties

--- LJ App/demo app/ReactFrontEnd/LJReactVite/src/components/home/Home.jsx
+++ LJ App/demo app/ReactFrontEnd/LJReactVite/src/components/home/Home.jsx
@@ -4,18 +4,18 @@ import "./home.css";
 const Home = () => {
   return (
     <div>
-      <div class="jumbotron">
-        <h1 class="display-4">Welcome, Cesily!</h1>
-        <p class="lead">
+      <div className="jumbotron">
+        <h1 className="display-4">Welcome, Cesily!</h1>
+        <p className="lead">
           This is a simple hero unit, a simple jumbotron-style component for
           calling extra attention to featured content or information.
         </p>
-        <hr class="my-4" />
+        <hr className="my-4" />
         <p>
           It uses utility classes for typography and spacing to space content
           out within the larger container.
         </p>
-        <a class="btn btn-primary btn-lg" href="#" role="button">
+        <a className="btn btn-primary btn-lg" href="#" role="button">
           Learn more
         </a>
       </div>
