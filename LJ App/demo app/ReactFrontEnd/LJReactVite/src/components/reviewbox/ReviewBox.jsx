bd5f1ed92a8d53cb953e8fbd20ea5adad620b986
Cesily Waters <96clondon@proton.me>
2024-02-07 23:24:13 -0500

    review box

---
+++ LJ App/demo app/ReactFrontEnd/LJReactVite/src/components/reviewbox/ReviewBox.jsx
@@ -0,0 +1,23 @@
+<div>
+  <div class="mb-3">
+    <label for="exampleFormControlInput1" class="form-label">
+      Title
+    </label>
+    <input
+      type="text"
+      class="form-control"
+      id="exampleFormControlInput1"
+      placeholder=""
+    />
+  </div>
+  <div class="mb-3">
+    <label for="exampleFormControlTextarea1" class="form-label">
+      Text area
+    </label>
+    <textarea
+      class="form-control"
+      id="exampleFormControlTextarea1"
+      rows="3"
+    ></textarea>
+  </div>
+</div>;
